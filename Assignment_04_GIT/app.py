f0fb226464d746e0bd303e82e78b0a00220f961a
Ranitha <ranithapatel@gmail.com>
2026-01-07 15:05:15 +0530

    Added Item ID field

--- Assignment_04_GIT/app.py
+++ Assignment_04_GIT/app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, request   # add request to import
+from pymongo import MongoClient    # only if you are using MongoDB
 
 app = Flask(__name__)
 
+# MongoDB setup (example)
+client = MongoClient("mongodb://localhost:27017/")
+db = client["your_database"]
+collection = db["todo_collection"]
+
+#  Home route
 @app.route('/')
 def home():
     return "Assignment 4 Flask App Running"
 
+#  Todo submission route
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo():
+    itemName = request.form['itemName']
+    itemDescription = request.form['itemDescription']
+    collection.insert_one({
+        "itemName": itemName,
+        "itemDescription": itemDescription
+    })
+    return "Todo Item Saved"
+
+#  Run the app
 if __name__ == '__main__':
     app.run(debug=True)
