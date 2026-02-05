5029d178fa203463aa092eb94430141360e1d9ab
Ranitha <159269083+RanithaPatel@users.noreply.github.com>
2026-02-05 18:08:25 +0530

    Update server.js

--- ASSIGNMENT-05/Frontend/server.js
+++ ASSIGNMENT-05/Frontend/server.js
@@ -1,1 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/submit', async (req, res) => {
+  try {
+    const response = await axios.post(
+      'http://backend:5000/submit',
+      req.body
+    );
+    res.send(response.data);
+  } catch (error) {
+    res.send("Error connecting to backend");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Frontend running on port 3000");
+});
 
