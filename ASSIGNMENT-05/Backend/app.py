4dda875ab1cd4ffc604b1078ad04a7e67332bc98
Ranitha <159269083+RanithaPatel@users.noreply.github.com>
2026-02-05 18:06:27 +0530

    Update app.py

--- ASSIGNMENT-05/Backend/app.py
+++ ASSIGNMENT-05/Backend/app.py
@@ -1,1 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    email = request.form.get('email')
+
+    return jsonify({
+        "message": "Data received successfully",
+        "name": name,
+        "email": email
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
