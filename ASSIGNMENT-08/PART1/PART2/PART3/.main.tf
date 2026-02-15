4b7cc8fb005b82df54766b9cc884dc31607d733d
Ranitha <159269083+RanithaPatel@users.noreply.github.com>
2026-02-15 11:11:02 +0530

    Update .main.tf

--- ASSIGNMENT-08/PART1/PART2/PART3/.main.tf
+++ ASSIGNMENT-08/PART1/PART2/PART3/.main.tf
@@ -1,15 +1,46 @@
-provider "aws" {
-  region = var.aws_region
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
 }
 
-resource "aws_ecr_repository" "flask" {
-  name = "flask-repo"
+provider "docker" {}
+
+resource "docker_image" "flask" {
+  name = "flask-app"
+  build {
+    context    = "."
+    dockerfile = "Dockerfile.flask"
+  }
 }
 
-resource "aws_ecr_repository" "express" {
-  name = "express-repo"
+resource "docker_container" "flask" {
+  name  = "flask-container"
+  image = docker_image.flask.latest
+  ports {
+    internal = 5000
+    external = 5000
+  }
 }
 
-resource "aws_ecs_cluster" "main" {
-  name = "app-cluster"
+resource "docker_image" "express" {
+  name = "express-app"
+  build {
+    context    = "."
+    dockerfile = "Dockerfile.express"
+  }
 }
+
+resource "docker_container" "express" {
+  name  = "express-container"
+  image = docker_image.express.latest
+  ports {
+    internal = 3000
+    external = 3000
+  }
+}
+
+
